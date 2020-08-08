672e4d07a2be530f2effb4dc8ed3456038af1639
Hans Petter Jansson <hpj@cl.no>
2020-08-08 20:42:49 +0200

    sixelparser: Tabs -> spaces

--- src/sixelparser.hh
+++ src/sixelparser.hh
@@ -29,43 +29,43 @@
 
 typedef unsigned short sixel_color_no_t;
 typedef struct sixel_image_buffer {
-	sixel_color_no_t *data;
-	int width;
-	int height;
-	int palette[DECSIXEL_PALETTE_MAX];
-	sixel_color_no_t ncolors;
-	int palette_modified;
-	int use_private_register;
+        sixel_color_no_t *data;
+        int width;
+        int height;
+        int palette[DECSIXEL_PALETTE_MAX];
+        sixel_color_no_t ncolors;
+        int palette_modified;
+        int use_private_register;
 } sixel_image_t;
 
 typedef enum parse_state {
-	PS_ESC = 1,   /* ESC */
-	PS_DECSIXEL,  /* DECSIXEL body part ", $, -, ? ... ~ */
-	PS_DECGRA,    /* DECGRA Set Raster Attributes " Pan; Pad; Ph; Pv */
-	PS_DECGRI,    /* DECGRI Graphics Repeat Introducer ! Pn Ch */
-	PS_DECGCI,    /* DECGCI Graphics Color Introducer # Pc; Pu; Px; Py; Pz */
+        PS_ESC = 1,   /* ESC */
+        PS_DECSIXEL,  /* DECSIXEL body part ", $, -, ? ... ~ */
+        PS_DECGRA,    /* DECGRA Set Raster Attributes " Pan; Pad; Ph; Pv */
+        PS_DECGRI,    /* DECGRI Graphics Repeat Introducer ! Pn Ch */
+        PS_DECGCI,    /* DECGCI Graphics Color Introducer # Pc; Pu; Px; Py; Pz */
 } parse_state_t;
 
 typedef struct parser_context {
-	parse_state_t state;
-	int pos_x;
-	int pos_y;
-	int max_x;
-	int max_y;
+        parse_state_t state;
+        int pos_x;
+        int pos_y;
+        int max_x;
+        int max_y;
 
         /* Pixel aspect ratio; unused */
-	int attributed_pan;
-	int attributed_pad;
+        int attributed_pan;
+        int attributed_pad;
 
-	int attributed_ph;
-	int attributed_pv;
-	int repeat_count;
-	int color_index;
-	int bgindex;
-	int param;
-	int nparams;
-	int params[DECSIXEL_PARAMS_MAX];
-	sixel_image_t image;
+        int attributed_ph;
+        int attributed_pv;
+        int repeat_count;
+        int color_index;
+        int bgindex;
+        int param;
+        int nparams;
+        int params[DECSIXEL_PARAMS_MAX];
+        sixel_image_t image;
 } sixel_state_t;
 
 int sixel_parser_init(sixel_state_t *st, int fgcolor, int bgcolor, int use_private_register);
